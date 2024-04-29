5e7b91fb5fff7a929f1c08655696b98833868e81
Hoa VM <hoavm.dev@gmail.com>
2024-04-30 05:25:51 +0700

    minor update

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import cors from 'cors';
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
 app.use('/api', api);
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
