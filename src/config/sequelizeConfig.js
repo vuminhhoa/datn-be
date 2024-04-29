958754c3b3d21338addb252f6bdaa4414e29da2d
Hoa VM <hoavm.dev@gmail.com>
2024-04-29 18:48:01 +0700

    config database

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -1,12 +1,13 @@
-import { Sequelize } from "sequelize";
-import "dotenv/config";
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
     logging: true,
-  },
+  }
 );
