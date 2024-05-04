cd8115b6a8a4b8f2c366f92d04a1b9540a0ff66a
Hoa VM <hoavm.dev@gmail.com>
2024-05-05 00:22:58 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import { rateLimit } from 'express-rate-limit';
 
 // app.use(limiter);
 app.set('trust proxy', 1)
-// app.use(cors({ origin: process.env.REACT_APP_BASE_URL }));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use('/api', api);
 
