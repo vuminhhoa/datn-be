22e916e3eed7543d149d94d5a57d2f6067610314
Hoa VM <hoavm.dev@gmail.com>
2024-05-05 00:07:36 +0700

    disabled cors

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.set('trust proxy', 1)
-app.use(cors({ origin: process.env.REACT_APP_BASE_URL }));
+// app.use(cors({ origin: process.env.REACT_APP_BASE_URL }));
 app.use(express.json({ limit: '50mb' }));
 app.use('/api', api);
 
