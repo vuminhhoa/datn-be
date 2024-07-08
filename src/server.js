14ba49d320897fe7e373d6aca6b41aa68ce5f9ec
Hoa VM <hoavm.dev@gmail.com>
2024-07-08 20:17:24 +0700

    minor đasa

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const limiter = rateLimit({
 });
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '50mb' }));
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
