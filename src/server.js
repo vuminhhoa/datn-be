4c0b1816564a45b7943e065e0000c6366e90b5f6
Hoa VM <hoavm.dev@gmail.com>
2024-07-08 22:16:00 +0700

    x

--- src/server.js
+++ src/server.js
@@ -15,6 +15,13 @@ const limiter = rateLimit({
 });
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bid.ibme.edu.vn');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 app.use(cors({ origin: 'https://bid.ibme.edu.vn', credentials: true }));
 app.use(express.json({ limit: '50mb' }));
 const server = http.createServer(app);
